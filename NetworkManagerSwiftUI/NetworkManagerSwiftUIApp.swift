bc9f4016c84d56a05e7704277b9eb2692410c65a
Brian McIntosh <brian@mixinteractive.com>
2023-03-08 19:31:07 -0500

    Initial Commit

---
+++ NetworkManagerSwiftUI/NetworkManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerSwiftUIApp.swift
+//  NetworkManagerSwiftUI
+//
+//  Created by Brian McIntosh on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
